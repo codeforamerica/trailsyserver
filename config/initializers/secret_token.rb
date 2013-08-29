458dbc1942b4cfde0ae5cce3b1c79b4a7153006a
Dan Avery <dan.github@danavery.com>
2013-08-29 13:58:33 -0700

    Initial rails/db config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trailsyserver::Application.config.secret_key_base = '6df059e660841305f058b96d3883b3d14242cf67fcff8c318ccce982236883f7ffc972d38e35d9448e0b7c04cc8554a74177bf8ea362d1a1f6ebbacffc29d739'
