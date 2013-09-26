b9f9beb98c58c819691f6ada3ddca223bbf8d0a9
Dan Avery <dan.github@danavery.com>
2013-09-25 21:48:39 -0700

    added cleanup of orphan photos after photo save

--- app/models/photorecord.rb
+++ app/models/photorecord.rb
@@ -1,4 +1,13 @@
 class Photorecord < ActiveRecord::Base
   has_attached_file :photo, :styles => { medium: "300x300>", thumb: "100x100>" }
   belongs_to :trail
+  after_save :clean_nils
+
+  def clean_nils
+    Photorecord.all.each do |photorecord|
+      if photorecord.trail_id.nil?
+        photorecord.destroy
+      end
+    end
+  end
 end
