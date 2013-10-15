56c08fd9ae2443cf051f9819e5d99a41ec5f79f0
Dan Avery <dan.github@danavery.com>
2013-10-15 10:29:18 -0700

    Initial config for Heroku PostGIS

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,28 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
+
 module Trailsyserver
   class Application < Rails::Application
 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,12 +15,20 @@ before_fork do |server, worker|
     ActiveRecord::Base.connection.disconnect!
 end  
 
-after_fork do |server, worker|
-
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+after_fork do |server, worker| 
+  if defined?(ActiveRecord::Base) 
+    config = Rails.application.config.database_configuration[Rails.env] 
+    config['adapter'] = 'postgis' 
+    ActiveRecord::Base.establish_connection(config)
+  end 
 end
+
+# after_fork do |server, worker|
+
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+#   end
+
+#   defined?(ActiveRecord::Base) and
+#     ActiveRecord::Base.establish_connection
+# end
